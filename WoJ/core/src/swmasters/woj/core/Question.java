9c51d8f99c3c619ae4526df64382c1cc7e07f8d2
Rob Miceli <rjm073@gmail.com>
2016-08-09 18:36:55 -0400

    Fix compilation errors in Questions.java

--- WoJ/core/src/swmasters/woj/core/Question.java
+++ WoJ/core/src/swmasters/woj/core/Question.java
@@ -5,11 +5,11 @@ public class Question {
 	private String answer;   /**< The text of the answer to the question */
 	private Integer pointValue; /**< The integer value of the points for the question */
 	private boolean answered; /**< The boolean for whether the question has been answered or not */
-	
+
 	public Question() {
-		
+
 	}
-	
+
 	public Question(String question, String answer, Integer pointValue, boolean answered) {
 		setQuestion(question);
 		setAnswer(answer);
@@ -20,7 +20,7 @@ public class Question {
 	public String getAnswer() {
 		return answer;
 	}
-	
+
 	public boolean getAnswered() {
 		return answered;
 	}
@@ -28,15 +28,19 @@ public class Question {
 	public Integer getpointValue() {
 		return pointValue;
 	}
-	
+
 	public String getQuestion() {
-		get question;
+		return question;
 	}
+
+    public void setPointValue(int pointValue) {
+        this.pointValue = pointValue;
+    }
 
 	public void setAnswer(String answer) {
 		this.answer = answer;
 	}
-	
+
 	public void setAnswered(boolean answered) {
 		this.answered = answered;
 	}
@@ -44,13 +48,14 @@ public class Question {
 	public void setpointValue(Integer pointValue) {
 		this.pointValue = pointValue;
 	}
-	
+
 	public void setQuestion(String question) {
 		this.question = question;
 	}
-	
+
 	public boolean verifyAnswer(String stringToVerify) {
 		/* Needs to be written */
+        return false;
 	}
 
 }
